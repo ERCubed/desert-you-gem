7b9e441bf482ed1e2b8a6e1fa03efa7293caeb3b
Eric Roberts <eric-roberts@pluralsight.com>
2024-09-11 14:29:32 -0600

    Spell constraints correctly...
    Signed-off-by: Eric Roberts <eric-roberts@pluralsight.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 DesertYouGem::Engine.routes.draw do
   match '*any',
     to: redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
-    contstraints: { format: 'php' },
+    constraints: { format: 'php' },
     via: [:get, :post, :patch, :delete]
 end
